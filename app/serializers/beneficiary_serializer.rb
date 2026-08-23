b03d9ada12e18736c093186335f6bf680b362f0f
Steve Smith <steve.smith@buddyrents.com>
2026-08-23 03:14:11 +0000

    Fix: la lista de quien recibe tiraba 500 si la migracion de kinship no ha corrido - atributo defensivo

--- app/serializers/beneficiary_serializer.rb
+++ app/serializers/beneficiary_serializer.rb
@@ -2,7 +2,13 @@ class BeneficiarySerializer
   include JSONAPI::Serializer
 
   attributes :id, :user_id, :name, :last_name, :email, :phone,
-             :address1, :address2, :zip_code, :state, :city, :kinship,
+             :address1, :address2, :zip_code, :state, :city,
              :created_at, :updated_at
+
+  # Defensivo: si la migración de kinship aún no corre, responder nil en lugar
+  # de tirar 500 (rompía la lista de quien recibe en el carrito).
+  attribute :kinship do |b|
+    b.respond_to?(:kinship) ? b.kinship : nil
+  end
 end
 
